94f836619da50d8396a40696dbce2d0c5590a604
Jackson <jackson.i@aluno.ifsc.edu.br>
2024-03-06 11:54:51 -0300

    terceiroCommit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,13 @@ import heroImg from '../../public/assets/hero.png'
 export default function Home() {
   return (
     <div className={styles.container}>
+
       <Head>
-        <title>Tarefas+ | Organize suas tarefas de foram fácil</title>
+        <title> DuoTask | Organize suas tarefas de foram fácil</title>
       </Head>
 
       <main className={styles.main}>
+
         <div className={styles.logoContent}>
           <Image
             className={styles.hero}
@@ -20,10 +22,20 @@ export default function Home() {
             priority
           />
         </div>
+
         <h1 className={styles.title}>
           Sistema feito para organizar <br/> 
           seus estudos e tarefas
         </h1>
+
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span> +12 posts</span>
+          </section>
+          <section className={styles.box}>
+            <span> +90 comentários</span>
+          </section>
+        </div>
       </main>
     </div>
 
