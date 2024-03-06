e8b302747321472b1db758d81ac73c617ec50b69
Jackson <jackson.i@aluno.ifsc.edu.br>
2024-03-06 17:12:28 -0300

    quartoCommit

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,38 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
 import styles from './styles.module.css';
 import Link from 'next/link';
 
-export function Header (){
+export function Header() {
+
+    const { data: session, status } = useSession();
+
     return (
         <header className={styles.header}>
             <section className={styles.content}>
                 <nav className={styles.nav}>
                     <Link href="/">
-                        <h1 className={styles.logo}> 
+                        <h1 className={styles.logo}>
                             DuoTask<span>+</span>
                         </h1>
                     </Link>
-                    <Link href="/dashboard" className={styles.link}>
-                        Meu Painel
-                    </Link>
+                    {session?.user && (
+                        <Link href="/dashboard" className={styles.link}>
+                            Meu Painel
+                        </Link>
+                    )}
                 </nav>
 
-                <button className={styles.loginButton}> Acessar </button>
+                {status === "loading" ? (
+                    <></>
+                ) : session ? (
+                    <button className={styles.loginButton} onClick={() => signOut()}>
+                        Olá {session?.user?.name}
+                    </button>
+                ) : (
+                    <button className={styles.loginButton} onClick={() => signIn("google")}>
+                        Acessar
+                    </button>
+                )}
             </section>
         </header>
     )
